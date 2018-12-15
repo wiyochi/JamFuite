f5e2d4e00701a551a9a165e55b6eec8f29b6e417
Louis Turpinat <turpinat.louis@gmail.com>
2018-12-15 22:02:29 +0100

    Adding ReaderStoryLine base

---
+++ sources/MiniGame/Readers/ReaderStoryLine.hpp
@@ -0,0 +1,12 @@
+#if !defined(READER_STORY_LINE__H)
+#define READER_STORY_LINE__H
+
+#include <rapidjson/document.h>
+#include <iostream>
+#include <fstream>
+
+class ReaderStoryLine
+{
+};
+
+#endif // READER_STORY_LINE__H
